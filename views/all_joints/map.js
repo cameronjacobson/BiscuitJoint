ace0c335a3596c731b68ba5adf11c87e06544a6b
Cameron Jacobson <cameron@datashovel.com>
2013-03-24 01:34:55 +0000

    updating all_joints view

--- views/all_joints/map.js
+++ views/all_joints/map.js
@@ -1,8 +1,10 @@
 function (doc) {
 	if (doc.class == 'BiscuitJoint\\JointDefinition') {
-		emit([doc.state.partA, doc.state.type, doc.state.partB], {_id:doc._id});
-		if(doc.state.symmetric){
-			emit([doc.state.partB, doc.state.type, doc.state.partA], {_id:doc._id});
+		if(doc.state.partA && doc.state.partB && doc.state.type){
+			emit([doc.state.partA, doc.state.type, doc.state.partB], {_id:doc._id});
+			if(doc.state.symmetric){
+				emit([doc.state.partB, doc.state.type, doc.state.partA], {_id:doc._id});
+			}
 		}
 	}
 }
